d5ee4ca22127e8f9bc010deedbc7d74dc3878069
udezueoluomachi <basilchimaobi2@gmail.com>
2024-05-16 19:56:02 +0100

    getting app setup

---
+++ color.config.js
@@ -0,0 +1,11 @@
+const Colors = {
+    "white-1" : "#FFFFFF",
+    "white-2" : "#FDFDFF",
+    "white-3" : "#E2E1DC",
+    "cream" : "#C6C5B9",
+    "blue-1" : "#62929E",
+    "ash" : "#546A7B",
+    "black-1" : "#393D3F"
+};
+
+export default Colors;
