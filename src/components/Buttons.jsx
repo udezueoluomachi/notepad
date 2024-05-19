4affe9f6c675dc0b5175c3f813d65b89c787e272
udezueoluomachi <basilchimaobi2@gmail.com>
2024-05-19 21:51:53 +0100

    progress

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,25 @@
+import {
+    Text,
+    Linking,
+    TouchableOpacity
+} from "react-native";
+
+export const Btn = props => {
+    return (
+        <TouchableOpacity style={props.style} onPress={props.onPress}>
+            <Text style={props.textStyle}>
+                {props.text}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+export const Anchor = props => {
+    return (
+        <TouchableOpacity style={props.style} onPress={() => Linking.openURL(props.href)}>
+            <Text style={props.textStyle}>
+                {props.text}
+            </Text>
+        </TouchableOpacity>
+    )
+}
