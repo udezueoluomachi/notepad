9be5dec33054e2b013d3865d583d6c0c339f1aa4
udezueoluomachi <basilchimaobi2@gmail.com>
2024-07-22 22:10:22 +0100

    fixed app theme

--- color.config.js
+++ color.config.js
@@ -2,7 +2,7 @@ const Colors = {
     "white-1" : "#FFFFFF",
     "white-2" : "#FDFDFF",
     "white-3" : "#E2E1DC",
-    "cream" : "#C6C5B9",
+    "cream" : "#eeeeee",
     "blue-1" : "#62929E",
     "ash" : "#546A7B",
     "black-1" : "#393D3F"
